752f00792f916f2a584befd1ad93a00dc5c00bb3
hv.feb <30828880+vanghtc@users.noreply.github.com>
2022-03-29 11:44:18 +0700

    Update Application.java

--- spring.secutiry.basic/src/main/java/com/example/spring/secutiry/basic/Application.java
+++ spring.secutiry.basic/src/main/java/com/example/spring/secutiry/basic/Application.java
@@ -9,7 +9,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("vanghtc update git --amend");
-		System.out.println("git fetch");
+		System.out.println("git fetch diff");
 	}
 
 }
